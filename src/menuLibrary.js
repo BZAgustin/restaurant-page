128fabecf6ea43ad0e5ac331bacb1e698c0b226d
BZAgustin <abustoszanelli@gmail.com>
2023-05-14 21:07:16 -0300

    Fix URLs

---
+++ src/menuLibrary.js
@@ -0,0 +1,39 @@
+const menu = [{
+  imgUrl: './assets/menu/lobster-risotto.jpg',
+  name: 'Lobster Risotto',
+  description: 'Creamy risotto cooked with lobster stock, chunks of succulent lobster meat and finished with grated Parmesan cheese, lemon zest, and chopped chives.',
+  price: 40,
+  side: 'left',
+}, {
+  imgUrl: './assets/menu/beef-wellington.jpg',
+  name: 'Beef Wellington',
+  description: 'Beef tenderloin seared and wrapped in puff pastry with a mixture of mushrooms, shallots, and herbs. Served with mashed potatoes and roasted carrots.',
+  price: 50,
+  side: 'right',
+}, {
+  imgUrl: './assets/menu/truffle-pasta.jpg',
+  name: 'Truffle Pasta',
+  description: 'Fresh pasta, heavy cream, Parmesan cheese and truffle oil. The truffle oil gives the dish its distinctive earthy flavor.',
+  price: 35,
+  side: 'left',
+}, {
+  imgUrl: './assets/menu/duck-breast.jpg',
+  name: 'Pan-seared Duck Breast',
+  description: 'Duck breast seared until crispy and finished in the oven, served with cherry sauce and garnished with fresh herbs.',
+  price: 45,
+  side: 'right',
+}, {
+  imgUrl: './assets/menu/lamb-chops.jpg',
+  name: 'Grilled Lamb Chops',
+  description: 'Grilled lamb seasoned with salt, pepper, and fresh rosemary. Served with a side of roasted asparagus and potatoes.',
+  price: 50,
+  side: 'left',
+}, {
+  imgUrl: './assets/menu/seared-scallops.jpg',
+  name: 'Seared Scallops',
+  description: 'Plump sea scallops served with a flavorful lemon-butter sauce and garnished with fresh herbs and a sprinkle of sea salt.',
+  price: 35,
+  side: 'right',
+}];
+
+export default menu;
